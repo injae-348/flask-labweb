c779790baf3a4af6d6bff7d58d20adf8b2df3057
정인재 <ant0593@naver.com>
2023-05-28 13:52:05 +0900

    modify the news part in index page

--- pybo/views/index_views.py
+++ pybo/views/index_views.py
@@ -2,6 +2,9 @@ from flask import Blueprint, render_template
 from pybo.views.auth_views import login_required
 bp = Blueprint('index',__name__,url_prefix='')
 
+from pybo.models import News
+
 @bp.route('/Index')
 def mainpage():
-    return render_template('Index/index.html')
+    newsList = News.query.order_by(News.activity_date.asc()).all()
+    return render_template('Index/index.html',newsList=newsList)
