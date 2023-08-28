8712dad83367b1f89e167796a0adec3ebf104568
injae-348 <ant0593@naver.com>
2023-08-28 17:32:16 +0900

    chage the pub page sort by date

--- pybo/views/pub_views.py
+++ pybo/views/pub_views.py
@@ -10,34 +10,34 @@ from pybo.views.auth_views import login_required
 
 @bp.route('/Publications_Domestic_Conferences')
 def PubDomConfDef():
-    publicationsList = Publications.query.order_by(Publications.create_date.asc())
+    publicationsList = Publications.query.order_by(Publications.date.asc())
     return render_template('Publications/pub_dom_conf.html',publicationsList=publicationsList)
 
 @bp.route('/Publications_Domestic_Journals')
 def PubDomJournDef():
-    publicationsList = Publications.query.order_by(Publications.create_date.asc())
+    publicationsList = Publications.query.order_by(Publications.date.asc())
     return render_template('Publications/pub_dom_journ.html',publicationsList=publicationsList)
 
 @bp.route('/Publications_Domestic_Patents')
 def PubDomPatenDef():
-    publicationsList = Publications.query.order_by(Publications.create_date.asc())
+    publicationsList = Publications.query.order_by(Publications.date.asc())
     return render_template('Publications/pub_dom_paten.html',publicationsList=publicationsList)
 
 
 
 @bp.route('/Publications_International_Conferences')
 def PubInConfDef():
-    publicationsList = Publications.query.order_by(Publications.create_date.asc())
+    publicationsList = Publications.query.order_by(Publications.date.asc())
     return render_template('Publications/pub_in_conf.html',publicationsList=publicationsList)
 
 @bp.route('/Publications_International_Journals')
 def PubInJournDef():
-    publicationsList = Publications.query.order_by(Publications.create_date.asc())    
+    publicationsList = Publications.query.order_by(Publications.date.asc())    
     return render_template('Publications/pub_in_journ.html',publicationsList=publicationsList)
 
 @bp.route('/Publications_International_Patents')
 def PubInPatenDef():
-    publicationsList = Publications.query.order_by(Publications.create_date.asc())
+    publicationsList = Publications.query.order_by(Publications.date.asc())
     return render_template('Publications/pub_in_paten.html',publicationsList=publicationsList)
 
 
