9d3ad5cefa25a935b83de652ebcb690b57ef981b
injae-348 <ant0593@naver.com>
2024-03-08 15:42:42 +0900

    Feat: add logs directory & handler

--- config/production.py
+++ config/production.py
@@ -1,5 +1,6 @@
 # 서버 환경
 from config.default import *
+from logging.config import dictConfig
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'))
 SQLALCHEMY_TRACK_MODIFICATIONS = False
@@ -7,3 +8,27 @@ SQLALCHEMY_TRACK_MODIFICATIONS = False
 # 시크릿 키
 # python -c "import os; print(os.urandom(16))" -> 으로 발급 받음
 SECRET_KEY = b'\xad|F6*ib%Y\xbf\xa4\x10s=\x85\x08'
+
+
+dictConfig({
+    'version': 1,
+    'formatters': {
+        'default': {
+            'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+        }
+    },
+    'handlers': {
+        'file': {
+            'level': 'INFO',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': os.path.join(BASE_DIR, 'logs/myproject.log'),
+            'maxBytes': 1024 * 1024 * 5,  # 5 MB
+            'backupCount': 5,
+            'formatter': 'default',
+        },
+    },
+    'root': {
+        'level': 'INFO',
+        'handlers': ['file']
+    }
+})
