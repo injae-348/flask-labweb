476407496b37b8d05cc574c420c6ad99bb36e6ce
injae-348 <ant0593@naver.com>
2024-03-18 20:21:24 +0900

    Feat : modify favicon logo to hi logo

--- config/production.py
+++ config/production.py
@@ -1,6 +1,6 @@
 # 서버 환경
 from config.default import *
-from logging.config import dictConfig
+# from logging.config import dictConfig
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'))
 SQLALCHEMY_TRACK_MODIFICATIONS = False
@@ -10,25 +10,25 @@ SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = b'\xad|F6*ib%Y\xbf\xa4\x10s=\x85\x08'
 
 
-dictConfig({
-    'version': 1,
-    'formatters': {
-        'default': {
-            'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
-        }
-    },
-    'handlers': {
-        'file': {
-            'level': 'INFO',
-            'class': 'logging.handlers.RotatingFileHandler',
-            'filename': os.path.join(BASE_DIR, 'logs/myproject.log'),
-            'maxBytes': 1024 * 1024 * 5,  # 5 MB
-            'backupCount': 5,
-            'formatter': 'default',
-        },
-    },
-    'root': {
-        'level': 'INFO',
-        'handlers': ['file']
-    }
-})
+# dictConfig({
+#     'version': 1,
+#     'formatters': {
+#         'default': {
+#             'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+#         }
+#     },
+#     'handlers': {
+#         'file': {
+#             'level': 'INFO',
+#             'class': 'logging.handlers.RotatingFileHandler',
+#             'filename': os.path.join(BASE_DIR, 'logs/myproject.log'),
+#             'maxBytes': 1024 * 1024 * 5,  # 5 MB
+#             'backupCount': 5,
+#             'formatter': 'default',
+#         },
+#     },
+#     'root': {
+#         'level': 'INFO',
+#         'handlers': ['file']
+#     }
+# })
--- pybo/views/main_views.py
+++ pybo/views/main_views.py
@@ -1,6 +1,5 @@
 from flask import Blueprint, url_for
 from werkzeug.utils import redirect
-
 bp = Blueprint('main',__name__,url_prefix='/')
 
 @bp.route('/hello')
@@ -10,3 +9,4 @@ def hello_pybo():
 @bp.route('/')
 def index():
     return redirect(url_for('index.mainpage'))
+
